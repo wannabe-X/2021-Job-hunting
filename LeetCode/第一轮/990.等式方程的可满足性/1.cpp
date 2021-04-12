a2d6f80f2e171e0316be1c2d24b4eafa58e4661e
“wannabe-X” <1432439968@qq.com>
2021-04-12 20:53:47 +0800

    code

---
+++ LeetCode/第一轮/990.等式方程的可满足性/1.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class UnionFind 
+{
+private:
+    int *parent;
+    int *size;
+
+public:
+    UnionFind (int n)
+    {
+        parent = new int(n);
+        size = new int(n);
+        for (int i = 0; i < n; i++)
+        {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int Find(int x)
+    {
+        while (x != parent[x])
+        {
+            parent[x] = parent[parent[x]];
+            x = parent[x];
+        }
+        return x;
+    }
+
+    void Union(int p, int q)
+    {
+        int rootP = Find(p);
+        int rootQ = Find(q);
+
+        if (rootP == rootQ)
+            return;
+
+        if (size[rootP] > size[rootQ])
+        {
+            parent[rootQ] = rootP;
+            size[rootP] += size[rootQ];
+        }
+        else
+        {
+            parent[rootP] = rootQ;
+            size[rootQ] += size[rootP];
+        }
+    }
+
+    bool Connected(int p, int q)
+    {
+        int rootP = Find(p);
+        int rootQ = Find(q);
+        return rootP == rootQ;
+    }
+};
+
+bool equationsPossible(vector<string> &equations)
+{
+    if (equations.size() < 2)
+        return true;
+
+    UnionFind  uf(26);
+    
+    for (string equ : equations)
+    {
+        if (equ.at(0) == '=')
+        {
+            cout << 2 << endl;
+            char x = equ.at(0), y = equ.at(3);
+            uf.Union(x - 'a', y - 'a');
+        }
+    }
+
+    for (string equ : equations)
+    {
+        if (equ.at(1) == '!')
+        {
+            char x = equ.at(0), y = equ.at(3);
+            if (uf.Connected(x - 'a', y - 'a'))
+                return false;
+        }
+    }
+
+    return true;
+}
+
+int main(void)
+{
+    vector<string> equtions = {"b==a", "a==b"};
+
+    bool ans = equationsPossible(equtions);
+
+    cout << boolalpha << ans << endl;
+}
