532711bd5fb687897778024c8b3f86c997d63c45
“wannabe-X” <1432439968@qq.com>
2021-04-13 23:01:13 +0800

    code

--- Sword/3.数组中重复的数字/3.cpp
+++ Sword/3.数组中重复的数字/3.cpp
@@ -15,7 +15,11 @@ int findRepeatNumber(vector<int> &nums)
         }
         if (nums[nums[i]] == nums[i])
             return nums[i];
-        swap(nums[i], nums[nums[i]]);
+        // 把元素nums[i]放到索引对应的位置上，例如
+        // nums[i] = 3,执行以下语句后，nums数组中，第四个元素即为3
+        // 每执行一次swap，就有一个元素被放到了索引对应的位置，被校正
+        // nums[nums[i]]就被放置了nums[i]
+        swap(nums[nums[i]], nums[i]);
     }
     return -1;
 }
