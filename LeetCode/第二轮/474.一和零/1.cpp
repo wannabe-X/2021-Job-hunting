242b56f5a7d6c2e4ba12267736353dd8d1a3df5b
“wannabe-X” <1432439968@qq.com>
2021-06-06 23:19:56 +0800

    code

--- LeetCode/第二轮/474.一和零/1.cpp
+++ LeetCode/第二轮/474.一和零/1.cpp
@@ -17,6 +17,14 @@ int getZeroOnes(string &str)
 int findMaxForm(vector<string> &strs, int m, int n)
 {
     int len = strs.size();
+    /*
+        res[i][j][k]的含义是在前i个字符串中，使用j个0和k个1的情况下最多可以得到的字符串数量。
+        假设数组 str 的长度为 len，则最终答案为 res[len][m][n]。
+
+        该题可以类比到01背包问题。
+        j可以当做装0的背包的容量
+        1可以当做装1的背包的容量
+    */
     vector<vector<vector<int>>> res(len + 1, vector<vector<int>>(m + 1, vector<int>(n + 1, 0)));
     for (int i = 1; i < len + 1; i++)
     {
@@ -27,10 +35,15 @@ int findMaxForm(vector<string> &strs, int m, int n)
         {
             for (int k = 0; k < n + 1; k++)
             {
-                res[i][j][k] = res[i - 1][j][k];
-                if (j >= zeros && k >= ones)
+                if (j < zeros || k < ones)
+                { 
+                    // 背包装不下
+                    res[i][j][k] = res[i - 1][j][k];
+                }
+                else if (j >= zeros && k >= ones)
                 {
-                    res[i][j][k] = max(res[i][j][k], res[i - 1][j - zeros][k - ones] + 1);
+                    // 背包可以装下，考虑是否需要装下当前字符串
+                    res[i][j][k] = max(res[i - 1][j][k], res[i - 1][j - zeros][k - ones] + 1);
                 }
             }
         }
