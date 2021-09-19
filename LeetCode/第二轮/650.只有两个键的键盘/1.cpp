ebeee2eb25e1b1b3b49b82bb3bcf838c6b54789d
“wannabe-X” <1432439968@qq.com>
2021-09-19 11:34:44 +0800

    code

--- LeetCode/第二轮/650.只有两个键的键盘/1.cpp
+++ LeetCode/第二轮/650.只有两个键的键盘/1.cpp
@@ -2,7 +2,7 @@
  * @Description: 650. 只有两个键的键盘
  * @Author: Mr. Lee
  * @Date: 2021-09-19 11:22:30
- * @LastEditTime: 2021-09-19 11:22:31
+ * @LastEditTime: 2021-09-19 11:33:20
  * @LastEditors: Mr. Lee
  */
 #include <iostream>
@@ -13,10 +13,27 @@ using namespace std;
 
 int minSteps(int n)
 {
+    vector<int> f(n + 1);
+    for (int i = 2; i <= n; ++i)
+    {
+        f[i] = INT_MAX;
+        for (int j = 1; j * j <= i; ++j)
+        {
+            if (i % j == 0)
+            {
+                f[i] = min(f[i], f[j] + i / j);
+                f[i] = min(f[i], f[i / j] + j);
+            }
+        }
+    }
+    return f[n];
 }
 
 int main(void)
 {
     int n = 3;
+    int ans = minSteps(n);
+    cout << ans << endl;
+    
     return 0;
 }
