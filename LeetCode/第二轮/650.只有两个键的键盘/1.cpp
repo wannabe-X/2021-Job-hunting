c633b953666502ae4756a4d0e5b904b4345f8f3c
“wannabe-X” <1432439968@qq.com>
2021-09-19 11:23:08 +0800

    code

---
+++ LeetCode/第二轮/650.只有两个键的键盘/1.cpp
@@ -0,0 +1,22 @@
+/*
+ * @Description: 650. 只有两个键的键盘
+ * @Author: Mr. Lee
+ * @Date: 2021-09-19 11:22:30
+ * @LastEditTime: 2021-09-19 11:22:31
+ * @LastEditors: Mr. Lee
+ */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+    int minSteps(int n) {
+
+    }
+
+int main(void)
+{
+    int n=3;
+    return 0;
+}
