bd046403ed0ad1bd979e7c9799f5ab80e2b5ffca
“wannabe-X” <1432439968@qq.com>
2021-10-10 13:08:24 +0800

    code

--- 秋招笔试/字节跳动笔试/1.cpp
+++ 秋招笔试/字节跳动笔试/1.cpp
@@ -2,6 +2,6 @@
  * @Description: 
  * @Author: Mr. Lee
  * @Date: 2021-10-10 09:51:46
- * @LastEditTime: 2021-10-10 09:51:47
+ * @LastEditTime: 2021-10-10 13:03:25
  * @LastEditors: Mr. Lee
  */
--- 秋招笔试/字节跳动笔试/4.cpp
+++ 秋招笔试/字节跳动笔试/4.cpp
@@ -1,7 +1,28 @@
 /*
- * @Description: 
+ * @Description: 博弈游戏
  * @Author: Mr. Lee
  * @Date: 2021-10-10 09:52:00
- * @LastEditTime: 2021-10-10 09:52:00
+ * @LastEditTime: 2021-10-10 13:08:06
  * @LastEditors: Mr. Lee
  */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+
+using namespace std;
+
+int main(void)
+{
+    int n;
+    double ans = 0.0;
+    cin >> n;
+    if (n % 2)
+        ans = double(n / 2 + 1) / n;
+    else
+        ans = 0.5;
+
+    printf("%.5f", ans);
+
+    return 0;
+}
