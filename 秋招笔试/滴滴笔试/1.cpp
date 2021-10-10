30a355106067e5a3c7c7b6f2999c675dd4df4970
“wannabe-X” <1432439968@qq.com>
2021-10-10 20:59:03 +0800

    code

--- 秋招笔试/滴滴笔试/1.cpp
+++ 秋招笔试/滴滴笔试/1.cpp
@@ -2,6 +2,35 @@
  * @Description: 
  * @Author: Mr. Lee
  * @Date: 2021-10-10 18:57:42
- * @LastEditTime: 2021-10-10 18:57:43
+ * @LastEditTime: 2021-10-10 19:39:03
  * @LastEditors: Mr. Lee
  */
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
+
+using namespace std;
+
+int func(vector<double> nums)
+{
+    sort(nums.begin(), nums.end());
+    unordered_set<double> st;
+    for (double num : nums)
+        st.insert(num);
+
+    return st.size();
+}
+
+int main(void)
+{
+    int N;
+    cin >> N;
+    vector<double> nums(N, 0);
+    for (int i = 0; i < N; i++)
+        cin >> nums[i];
+    int ans = func(nums);
+    cout << ans << endl;
+
+    return 0;
+}
--- 秋招笔试/滴滴笔试/2.cpp
+++ 秋招笔试/滴滴笔试/2.cpp
@@ -2,6 +2,61 @@
  * @Description: 
  * @Author: Mr. Lee
  * @Date: 2021-10-10 18:57:47
- * @LastEditTime: 2021-10-10 18:57:47
+ * @LastEditTime: 2021-10-10 20:58:42
  * @LastEditors: Mr. Lee
  */
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+
+using namespace std;
+
+bool check(vector<char> &v, int l, int r)
+{
+    stack<char> stk;
+
+    for (int i = l; i < r; i++)
+    {
+        if (!stk.empty() && ((stk.top() == '0' && v[i] == '1') || (stk.top() == '2' && v[i] == '3')))
+        {
+            stk.pop();
+        }
+        else
+        {
+            stk.push(v[i]);
+        }
+    }
+
+    return stk.empty();
+}
+
+int func(vector<char> &v)
+{
+    int res = -1;
+    for (int i = 0; i < v.size(); i++)
+    {
+        for (int j = i + 1; j <= v.size(); j++)
+        {
+            if (j - i < res)
+                continue;
+            if (check(v, i, j))
+                res = max(res, j - i);
+        }
+    }
+
+    return res;
+}
+
+int main(void)
+{
+    int n;
+    cin >> n;
+    vector<char> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+    int ans = func(v);
+    cout << ans << endl;
+
+    return 0;
+}
