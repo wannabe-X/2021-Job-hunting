9f3d7a45694429383ce7ff06024562ae071d05b7
“wannabe-X” <1432439968@qq.com>
2021-11-25 00:38:35 +0800

    code

---
+++ LeetCode/第二轮/458.可怜的小猪/1.cpp
@@ -0,0 +1,28 @@
+/*
+ * @Description: 458. 可怜的小猪
+ * @Author: Mr. Lee
+ * @Date: 2021-11-25 00:12:05
+ * @LastEditTime: 2021-11-25 00:32:27
+ * @LastEditors: Mr. Lee
+ */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int poorPigs(int buckets, int minutesToDie, int minutesToTest)
+{
+    int base = minutesToTest / minutesToDie + 1;
+    return ceil(log10(buckets) / log10(base));
+}
+
+int main(int argc, char *argv[])
+{
+    int buckets = 1000, minutesToDie = 15, minutesToTest = 60;
+    int ans = poorPigs(buckets, minutesToDie, minutesToTest);
+    cout << ans << endl;
+
+    return 0;
+}
