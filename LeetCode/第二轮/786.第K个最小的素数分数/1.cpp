e2e3308958cb9dee5bcf60cbaf3fc51e19ee4b63
“wannabe-X” <1432439968@qq.com>
2021-11-29 00:52:05 +0800

    code

---
+++ LeetCode/第二轮/786.第K个最小的素数分数/1.cpp
@@ -0,0 +1,37 @@
+/*
+ * @Description: 786.第K个最小的素数分数
+ * @Author: Mr. Lee
+ * @Date: 2021-11-29 00:44:09
+ * @LastEditTime: 2021-11-29 00:50:20
+ * @LastEditors: Mr. Lee
+ */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> kthSmallestPrimeFraction(vector<int> &arr, int k)
+{
+    vector<pair<int, int>> v;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = i + 1; j < arr.size(); j++)
+            v.emplace_back(arr[i], arr[j]);
+    }
+
+    sort(v.begin(), v.end(), [](const pair<int, int> &x, const pair<int, int> &y)
+         { return x.first * y.second < y.first * x.second; });
+
+    return {v[k - 1].first, v[k - 1].second};
+}
+
+int main(int argc, char *argv[])
+{
+    vector<int> arr = {1, 2, 3, 5};
+    int k = 3;
+    vector<int> ans = kthSmallestPrimeFraction(arr, k);
+    cout << ans[0] << " " << ans[1] << endl;
+
+    return 0;
+}
