730ee8ec6976962e8cb4b18c185aa580d464c8f7
“wannabe-X” <1432439968@qq.com>
2021-12-13 18:23:28 +0800

    code

--- C++数据结构设计题/自旋锁的实现/1.cpp
+++ C++数据结构设计题/自旋锁的实现/1.cpp
@@ -1,26 +1,29 @@
 /*
- * @Description: 
+ * @Description:
  * @Author: Mr. Lee
  * @Date: 2021-12-13 18:13:40
- * @LastEditTime: 2021-12-13 18:13:41
+ * @LastEditTime: 2021-12-13 18:17:25
  * @LastEditors: Mr. Lee
  */
 #include <atomic>
 using namespace std;
 
-class spin_lock {
+class spin_lock
+{
 private:
-    atomic<bool> flag{false}; //false不加锁、true加锁
+    atomic<bool> flag{false}; // false不加锁、true加锁
 public:
-    spin_lock() = default;
-    spin_lock(const spin_lock&) = delete;
-    spin_lock& operator=(const spin_lock&) = delete;
-    void lock() {
+    spin_lock() = default;                 // 使用默认的构造函数
+    spin_lock(const spin_lock &) = delete; // 禁止使用拷贝构造函数
+    spin_lock &operator=(const spin_lock &) = delete; // 禁止使用赋值构造
+    void lock()
+    {
         bool expected = false;
         while (!flag.compare_exchange_strong(expected, true))
             expected = false;
     }
-    void unlock() {
+    void unlock()
+    {
         flag.store(false);
     }
 };
